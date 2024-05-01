0b3aad81876eb11542b1fd0f594b1002f6d80d59
Pema Sherpa <pematamang111@gmail.com>
2024-04-30 23:09:54 -0400

    add crud to save in db

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -16,7 +16,21 @@ const initdb = async () =>
 export const putDb = async (content) => {
   console.error('putDb not implemented');
 
+// Create a connection to the database database and version we want to use.
+const todosDb = await openDB('jate', 1);
 
+// Create a new transaction and specify the database and data privileges.
+const tx = todosDb.transaction('jate', 'readwrite');
+
+// Open up the desired object store.
+const store = tx.objectStore('jate');
+
+// Use the .add() method on the store and pass in the content.
+const request = store.add({ todo: content });
+
+// Get confirmation of the request.
+const result = await request;
+console.log('Data saved to the database', result);
   
 }
 
