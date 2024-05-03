a672e63f232d02d28383cdbce573d53d886a1727
Pema Sherpa <pematamang111@gmail.com>
2024-05-02 22:26:45 -0400

    fix typos in putdb

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -24,13 +24,16 @@ const tx = todosDb.transaction('jate', 'readwrite');
 
 // Open up the desired object store.
 const store = tx.objectStore('jate');
+const timeStamp = Date.now();
+await store.put({content: timeStamp});
+await tx.done;
+console.log('content added');
+// // Use the .add() method on the store and pass in the content.
+// const request = store.add({ todo: content });
 
-// Use the .add() method on the store and pass in the content.
-const request = store.add({ todo: content });
-
-// Get confirmation of the request.
-const result = await request;
-console.log('Data saved to the database', result);
+// // Get confirmation of the request.
+// const result = await request;
+// console.log('Data saved to the database', result);
   
 }
 
